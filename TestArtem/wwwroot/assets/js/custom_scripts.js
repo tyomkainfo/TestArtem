edffa046ce05db90f5584b2bc45a44e481e0433e
tyomka.info <tyomka.info@gmail.com>
2024-09-26 08:30:57 +0200

    done some task 26/09

--- TestArtem/wwwroot/assets/js/custom_scripts.js
+++ TestArtem/wwwroot/assets/js/custom_scripts.js
@@ -3,19 +3,4 @@ $(document).ready(function () {
     $('#mainCheckbox').click(function () {
         $('.childCheckbox').prop('checked', this.checked);
     });
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        autoplay: true,
-        autoplayTimeout: 2000,
-        responsive: {
-            0: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        }
-    });
 });
