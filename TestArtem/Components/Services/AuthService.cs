32d84371c12e5aa74977a9c5651d06e823a15993
artem.ryzhov <artem.ryzhov@EFLSWS24>
2024-10-01 11:47:29 +0200

    add some changes 01/10

--- TestArtem/Components/Services/AuthService.cs
+++ TestArtem/Components/Services/AuthService.cs
@@ -1,29 +1,44 @@
 ﻿using System;
+using Microsoft.JSInterop;
 
 namespace TestArtem.Components.Services
 {
     public class AuthService
     {
         private bool isAuthenticated;
+        private readonly IJSRuntime jsRuntime;
+
+        public AuthService(IJSRuntime jsRuntime)
+        {
+            this.jsRuntime = jsRuntime;
+        }
 
         public bool IsAuthenticated => isAuthenticated;
 
-        public void Login(string username, string password)
+        public async Task InitializeAuthenticationStateAsync()
+        {
+            var authState = await jsRuntime.InvokeAsync<string>("localStorage.getItem", "isAuthenticated");
+            isAuthenticated = authState == "true";
+        }
+
+        public async Task Login(string username, string password)
         {
             // В реальном приложении проверяйте учетные данные с помощью базы данных или хранилища
             if (username == "test" && password == "test")
             {
                 isAuthenticated = true;
+                await jsRuntime.InvokeVoidAsync("localStorage.setItem", "isAuthenticated", "true");
             }
             else
             {
-                throw new UnauthorizedAccessException("Invalid credentials"); // Более специфичное исключение
+                throw new UnauthorizedAccessException("Invalid credentials");
             }
         }
 
-        public void Logout()
+        public async Task Logout()
         {
             isAuthenticated = false;
+            await jsRuntime.InvokeVoidAsync("localStorage.removeItem", "isAuthenticated");
         }
 
         public bool IsUserLoggedIn()
